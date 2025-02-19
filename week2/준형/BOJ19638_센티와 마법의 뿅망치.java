238a6dcfaa45514841333f3a075cd7e5ed610f0a
BAMLATTE <abide0206@naver.com>
2025-02-19 18:35:05 +0900

    Update BOJ19638_센티와 마법의 뿅망치.java

--- week2/준형/BOJ19638_센티와 마법의 뿅망치.java
+++ week2/준형/BOJ19638_센티와 마법의 뿅망치.java
@@ -1,57 +1,44 @@
-package 준형;
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
-class Main {
-
-    static int N;
-    static List<Integer>[] tree;
-    static int[] parent;
-    static boolean[] visited;
-
-    static void dfs(int node) {
-        visited[node] = true;
-
-        for (int next : tree[node]) {
-            if (!visited[next]) {
-                parent[next] = node;
-                dfs(next);
-            }
-        }
-    }
-
-    public static void main(String args[]) throws Exception {
+public class Main {
+    
+	static int N;
+	static int H;
+	static int T;
+	
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-
-        tree = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++ ) {
-            tree[i] = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < N; i++) {
+        	pq.add(Integer.parseInt(br.readLine()));
         }
-
-        for (int i = 0; i < N - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            tree[a].add(b);
-            tree[b].add(a);
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < T; i++) {
+        	if (!pq.isEmpty()) {
+        		int largeMan = pq.peek();
+                if (largeMan < H) break;
+        		if (largeMan > 1) {
+        			pq.add(pq.poll() / 2);
+        			cnt++;
+        		}
+        	}
         }
-
-        parent = new int[N + 1];
-        visited = new boolean[N + 1];
-
-        dfs(1);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 2; i <= N; i++) {
-            sb.append(parent[i]).append("\n");
+        
+        if (pq.peek() >= H) {
+        	System.out.println("NO");
+        	System.out.println(pq.peek());
+        } else {
+        	System.out.println("YES");
+        	System.out.println(cnt);
         }
-        System.out.println(sb);
     }
 }
