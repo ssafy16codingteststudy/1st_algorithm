7ed2d28e40491326ef9431ae2321166f308d9638
JSY <jsy98515@gmail.com>
2025-02-19 15:07:44 +0900

    정세연 : BOJ 1351 무한 수열

---
+++ week2/세연/BOJ1351_무한수열.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Map<Long, Long> dp = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long P = Long.parseLong(st.nextToken());
+        long Q = Long.parseLong(st.nextToken());
+        dp.put(0L,1L);
+        System.out.println(getVal(N,P,Q));
+    }
+    static long getVal(long idx, long P, long Q){
+        if (!dp.containsKey(idx)) {
+            dp.put(idx, getVal(idx/P,P,Q) + getVal(idx/Q,P,Q));
+        }
+        return dp.get(idx);
+
+    }
+}
