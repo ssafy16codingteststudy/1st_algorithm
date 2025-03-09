352877cd8226bd0c1e0a65224fe90c62333adca2
eunseongman <99581773+eunseongman@users.noreply.github.com>
2025-03-09 13:25:42 +0900

    이은성 :  SWEA_Pro3_승강제리그 주석 수정

--- week5/은성/SWEA_Pro3_승강제리그.java
+++ week5/은성/SWEA_Pro3_승강제리그.java
@@ -4,8 +4,8 @@ public class UserSolution {
 
     private int N;
     private int L;
-    private int leagueSize;
-    private List<TreeSet<Integer>> leagueList;
+    private int leagueSize;                       // N / L 값을 저장
+    private List<TreeSet<Integer>> leagueList;    // 각 리그를 TreeSet으로 저장, 전체 리그는 List에서 관리
 
     void init(int N, int L, int mAbility[]) {
         this.N = N;
@@ -22,6 +22,9 @@ public class UserSolution {
                 int id = (leagueSize * i) + j;
                 int ability = mAbility[id];
                 // 좌표 압축
+                // 이렇게 한 이유? Player class 를 생성하는 것 보다 시간 절약이 될 것이라 생각했음
+                // 하지만 실제로 테스트 해보니, 드라마틱한 시간 절약이 되진 않았다.
+                // (약 1.6초 -> 1.5초)
                 league.add(ability * N + (N - (id + 1)));
             }
         }
@@ -50,10 +53,10 @@ public class UserSolution {
             Integer player = league.pollFirst();
             nextLeague.add(player);
 
-            // 못하는 선수 넣기
+            // 2. 못하는 선수 넣기
             league.add(deque.pollFirst());
 
-            // 좌표 반환하여 sum 에 저장
+            // 3. 좌표 반환하여 sum 에 저장
             sumId += (N - (player % N)) - 1;
         }
 
@@ -68,6 +71,7 @@ public class UserSolution {
         for (int i = 0; i < L - 1; i++) {
             TreeSet<Integer> league = leagueList.get(i);
 
+            // iterator 접근을 하면, treeSet도 순차 접근이 가능하다. (leagueSize / 2) 번만에 중간값 찾기 가능
             Iterator<Integer> iterator = league.iterator();
             Integer player = 0;
             for (int j = 0; j < (leagueSize + 1) / 2; j++) {
