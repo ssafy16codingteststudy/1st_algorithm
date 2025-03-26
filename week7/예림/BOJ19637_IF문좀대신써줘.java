abe1d34525361f9fcd2ef356357a83df3e7902c2
J0YERIM <chosarah70@gmail.com>
2025-03-26 17:34:40 +0900

    조예림 : BOJ_2512_예산

--- week7/예림/BOJ19637_IF문좀대신써줘.java
+++ week7/예림/BOJ19637_IF문좀대신써줘.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// standards[i] >= target를 처음으로 만족하는 i 찾기
 public class BOJ19637_IF문좀대신써줘 {
 
     private static int N, M;
