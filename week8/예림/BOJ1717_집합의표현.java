3ea3c570c2a94f986d75e38df6250269a2645d76
조예림 <96174711+joyerimi@users.noreply.github.com>
2025-04-01 11:38:53 +0900

    조예림 : BOJ_1717_집합의표현

---
+++ week8/예림/BOJ1717_집합의표현.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] parents;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parents = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parents[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            if (command == 0) {
+                union(u, v);
+            } else if (command == 1) {
+                if (find(u) == find(v)) {
+                    sb.append("YES\n");
+                } else {
+                    sb.append("NO\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int find(int x) {
+        if (parents[x] == x) {
+            return x;
+        }
+        return parents[x] = find(parents[x]);
+    }
+
+    private static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            parents[rootB] = rootA;
+        }
+    }
+}
