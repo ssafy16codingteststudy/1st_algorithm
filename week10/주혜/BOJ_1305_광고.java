a7b064f72a924ff226661ba02e2b8fbd06018449
juhye87 <78808933+juhye87@users.noreply.github.com>
2025-04-15 18:27:05 +0900

    김주혜: BOJ 1305 광고

---
+++ week10/주혜/BOJ_1305_광고.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[] pattern = br.readLine().toCharArray();
+
+        int pLength = pattern.length;
+
+        int[] pi = new int[pLength];
+        for (int i = 1, j = 0; i < pLength; i++) {
+            while (j > 0 && pattern[i] != pattern[j]) {
+                j = pi[j - 1];
+            }
+
+            if (pattern[i] == pattern[j]) {
+                pi[i] = ++j;
+            } else {
+                pi[i] = 0;
+            }
+        }
+
+        System.out.println(N - pi[pLength - 1]);
+
+    }
+}
